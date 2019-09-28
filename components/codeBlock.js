98940a0268c25fc244b2bfeffd7d14529c35bdfe
Zachary Sohovich <zachsohovich@thebriga.de>
2019-09-27 17:00:38 -0700

    fixed import issue

--- components/codeBlock.js
+++ components/codeBlock.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { dracula } from '../node_modules/react-syntax-highlighter/dist/esm/styles/hljs'
 
 const CodeBlock = props => {
-  console.log(props);
   return (
-    <SyntaxHighlighter language={props.language}>
+    <SyntaxHighlighter language={props.language} style={dracula}>
       {props.value}
     </SyntaxHighlighter>
   );
