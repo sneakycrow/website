e65058d0e6c2bb95cac8a0822b00b42e8b21e18f
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-09-28 21:19:22 -0700

    added post styling

--- pages/post.js
+++ pages/post.js
@@ -2,40 +2,119 @@ import React, { Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 import fetch from 'isomorphic-unfetch';
+import Router from 'next/router';
 
 import Nav from '../components/nav';
 import CodeBlock from '../components/codeBlock';
 
 const StyledPost = styled.div`
-  width: ${props => props.theme.layout.contentMaxWidth};
+  max-width: ${props => props.theme.layout.contentMaxWidth};
+  width: 100%;
   margin: auto;
   display: flex;
   flex-direction: column;
   margin-top: 100px;
-`
+  padding: 0 16px;
+  p {
+    font-size: 1.7em;
+    line-height: 1.4em;
+    margin-bottom: 0.8em;
+  }
+  code {
+    background-color: ${props => props.theme.palette.lightGray};
+    border-radius: 2px;
+    padding-left: 4px;
+    padding-right: 4px;
+    font-size: 0.9em;
+    font-style: italic;
+  }
+`;
+
+const StyledStickyNav = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  pointer-events: none;
+  width: 100%;
+  div {
+    max-width: ${props => props.theme.layout.contentMaxWidth};
+    width: 100%;
+    margin: auto;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+`;
+
+const StyledBackButton = styled.button`
+  color: ${props => props.theme.palette.black};
+  pointer-events: all;
+  text-decoration: none;
+  transition: color 0.25s ease-in-out;
+  border: none;
+  border-radius: 2px;
+  display: inline-block;
+  font-weight: bold;
+  align-self: flex-start;
+  display: flex;
+  align-items: center;
+  font-size: 1em;
+  background-color: ${props => props.theme.palette.white};
+  margin: 8px;
+  padding: 8px 16px;
+  transition: border 0.25s ease-in-out;
+  pointer-events: all;
+  &:hover {
+    color: ${props => props.theme.palette.green};
+    cursor: pointer;
+  }
+  &::before {
+    content: '\\2190 Back';
+  }
+  @media screen and (max-width: ${props => props.theme.layout.mobileMaxWidth}) {
+    border-radius: 50%;
+    background-color: ${props => props.theme.layout.black};
+    width: 50px;
+    height: 50px;
+    &::before {
+      content: '\\2190';
+      line-height: 1.5em;
+    }
+  }
+`;
 
 const Post = ({ post }) => {
   return (
     <Fragment>
       <Nav />
-      <StyledPost>
+      <StyledPost className="markdown-body">
         <h1>{post.title}</h1>
-        <ReactMarkdown
-          source={post.body}
-          renderers={{ code: CodeBlock }}
-        />
+        <ReactMarkdown source={post.body} renderers={{ code: CodeBlock }} />
       </StyledPost>
+      <StyledStickyNav>
+        <div>
+          <StyledBackButton onClick={() => Router.back()} />
+        </div>
+      </StyledStickyNav>
     </Fragment>
-  )
-}
+  );
+};
 
 Post.getInitialProps = async ({ query }) => {
-  const res = await fetch(`https://cors-anywhere.herokuapp.com/https://write.as/api/collections/sneakycrow/posts/${query.slug}`);
+  const res = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://write.as/api/collections/sneakycrow/posts/${query.slug}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Origin: 'https://sneakycrow.dev'
+      }
+    }
+  );
   const data = await res.json();
 
   return {
     post: data.data
   };
-}
+};
 
 export default Post;
--- utils/theme.js
+++ utils/theme.js
@@ -3,6 +3,7 @@ const palette = {
     black: '#000000',
     white: '#FFFFFF',
     lightGray: '#E3E3E3',
+    lighterGray: '#FAFAFA',
     green: '#28B155'
   },
   dark: {
