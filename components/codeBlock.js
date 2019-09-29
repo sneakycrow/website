0cfcf8b390e8de624710951c86ed85ad1a0bbc07
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-09-28 20:02:59 -0700

    updated code block with new prism renderer

--- components/codeBlock.js
+++ components/codeBlock.js
@@ -1,12 +1,42 @@
-import React, { Fragment } from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dracula } from '../node_modules/react-syntax-highlighter/dist/esm/styles/hljs'
+import React from 'react';
+import styled from 'styled-components';
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
+import Prism from './prism';
+
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+
+  & .token-line {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
+`
+
+const LineNo = styled.span`
+  display: inline-block;
+  width: 2em;
+  user-select: none;
+  opacity: 0.3;
+`
 
 const CodeBlock = props => {
+  console.log(props.language);
   return (
-    <SyntaxHighlighter language={props.language} style={dracula}>
-      {props.value}
-    </SyntaxHighlighter>
+    <Highlight {...defaultProps} Prism={Prism} theme={theme} code={props.value} language={props.language}>
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <Pre className={className} style={style}>
+        {tokens.map((line, i) => (
+          <div {...getLineProps({ line, key: i })}>
+            <LineNo>{i + 1}</LineNo>
+            {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
+          </div>
+        ))}
+      </Pre>
+    )}
+  </Highlight>
   );
 };
 
