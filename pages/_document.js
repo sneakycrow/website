f6f018b35c77dd14c6c999be2ecf59a2a23b10cb
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-09-28 21:19:11 -0700

    added more font styles

--- pages/_document.js
+++ pages/_document.js
@@ -16,10 +16,9 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700|Open+Sans:400,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700|Open+Sans:400,700|Space+Mono&display=swap"
             rel="stylesheet"
           />
-
           {this.props.styleTags}
         </Head>
         <body>
