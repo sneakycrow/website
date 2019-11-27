a0b0e598c1965928b54fb21c795637b941a308f3
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-11-27 13:06:38 -0800

    added client id to env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     react_app_graphql_key: process.env.react_app_graphql_key,
-    react_app_graphql_api: process.env.react_app_graphql_api
+    react_app_graphql_api: process.env.react_app_graphql_api,
+    react_app_twitch_client_id: process.env.react_app_twitch_client_id
   }
 }
