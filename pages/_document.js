0667f749c359136998981e5294bf63d69b1f202a
Zachary E Sohovich <zach@sneakycrow.dev>
2020-01-17 18:04:54 -0800

    fix ga

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,20 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+// thank you @koodiklin
+// https://github.com/koodiklinikka/koodiklinikka.fi/commit/e27c50601d728b3b20320ffcc021a81657d8d4d6#diff-664264179f734c3055eba103c71ad479
+const trackPageView = () => {
+  if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+    return;
+  }
+  if (!window.GA_INITIALIZED) {
+    ReactGA.initialize("UA-58806132-1");
+    window.GA_INITIALIZED = true;
+  }
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 export default class MyDocument extends Document {
   static getInitialProps = ({ renderPage }) => {
     const sheet = new ServerStyleSheet();
@@ -10,6 +24,10 @@ export default class MyDocument extends Document {
 
     return { ...page, styleTags };
   };
+
+  componentDidMount() {
+    trackPageView();
+  }
 
   render() {
     return (
