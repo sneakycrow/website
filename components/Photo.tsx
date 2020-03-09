838b7fd272853835091c8c7b4c484c5b2b6ef5b8
Zachary E Sohovich <zach@sneakycrow.dev>
2020-03-08 20:13:03 -0700

    makes photos full width

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Photo = ({ source, caption, timestamp }) => (
-    <figure className="mt-4 mb-16">
-      <img className="shadow-md max-h-screen" src={source} alt={caption} />
+    <figure className="mt-4 mb-16 flex flex-col justify-center">
+      <img className="shadow-md min-w-full" src={source} alt={caption} />
       <figcaption className="mt-2">
         <span className="float-left">{caption}</span>
         <span className="float-right">{timestamp}</span>
