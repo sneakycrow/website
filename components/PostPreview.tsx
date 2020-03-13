8d3ac6aa5f59a4f322c9980ed47bf1e7673ca472
Zachary E Sohovich <zach@sneakycrow.dev>
2020-03-12 22:38:44 -0700

    fixed blog alignment

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const PostPreview = ({ title, timestamp }) => {
   return (
-    <div className="flex items-center items-center h-16 mb-1 shadow-sm post-preview">
-      <span className="text-sm mr-4 italic">{timestamp}</span>
-      <p className="text-lg w-2/3">{title}</p>
+    <div className="flex content-center items-start h-16 mb-4 shadow-sm post-preview">
+      <span className="text-sm mr-4 italic w-1/4">{timestamp}</span>
+      <p className="text-lg w-2/3 leading-tight">{title}</p>
     </div>
   )
 }
