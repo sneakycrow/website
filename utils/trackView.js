c88ac04c071eb4a94f83fe5b1aff137456ef7a32
Zachary E Sohovich <zach@sneakycrow.dev>
2020-05-07 21:01:26 -0700

    updated tracked url with referrer var

--- utils/trackView.js
+++ utils/trackView.js
@@ -4,8 +4,9 @@ import { INSERT_GENERAL_PAGE_VIEW_MUTATION } from '../lib/queries';
 const trackView = trackedUrl => {
   const regex = /sneakycrow.dev/g;
   const currentURL = window.location.toString() || '';
+  const referrerURL = document.referrer?.length > 0 ? document.referrer : null;
   if (regex.test(currentURL) && trackedUrl) {
-    return withData(INSERT_GENERAL_PAGE_VIEW_MUTATION, { url: trackedUrl });
+    return withData(INSERT_GENERAL_PAGE_VIEW_MUTATION, { url: trackedUrl, referrer: referrerURL });
   }
 }
 
