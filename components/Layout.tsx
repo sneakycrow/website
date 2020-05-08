7ffc74fae77f6962bc429e76eea8c5174e7c8319
Zachary E Sohovich <zach@sneakycrow.dev>
2020-05-07 21:01:17 -0700

    removed content-type meta tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,7 +13,6 @@ const Layout = (props: LayoutProps) => (
       <title>{props.title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       {props.description && (
         <meta name="description" content={props.description} />
       )}
