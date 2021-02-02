8e06b9db5545258c77da1a73c15d5011399d019f
Zachary E Sohovich <zach@sneakycrow.dev>
2021-02-02 06:34:16 -0800

    added migration alert

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,9 @@ export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
+      <Alert>
+        Website migration in progress. Contact me directly here: <a href="mailto:zach@sneakycrow.dev">zach@sneakycrow.dev</a>
+      </Alert>
       <div className="min-h-screen">
         <main>{children}</main>
       </div>
