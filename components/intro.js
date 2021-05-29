2d00d6d544e3de634e828acf89674fee660d4f7b
Zachary Sohovich <zach@sneakycrow.dev>
2021-05-29 16:49:43 -0700

    added video page, updated favicon

--- components/intro.js
+++ components/intro.js
@@ -1,10 +1,10 @@
-export default function Intro() {
+export default function Intro({ subtitle = "engineer and artist"}) {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         sneakycrow
       </h1>
-      <h4 className="text-center md:text-left text-3xl mt-5 md:pl-8">engineer and artist</h4>
+      <h4 className="text-center md:text-left text-3xl mt-5 md:pl-8">{subtitle}</h4>
     </section>
   );
 }
---
+++ pages/videos.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import dynamic from "next/dynamic";
+import Intro from "../components/intro";
+
+const Player = dynamic(
+    () => {
+        return import("../components/VideoPlayer");
+    },
+    {ssr: false}
+);
+
+const Videos = ({HLSURLList = []}) => {
+    return (
+        <div className="flex flex-col items-center">
+            <Intro subtitle={"videos and clips"}/>
+
+            {HLSURLList.map((url, index) => (
+                <div className="w-1/2 my-20">
+                    <Player url={url} key={`hls-${index}`}/>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+    const res = await fetch("https://yf537ok2eb.execute-api.us-east-1.amazonaws.com/default/dionysus-consume", {
+        headers: {
+            "X-Api-Key": "J3ORQvoTHZ9iuPDAaPxAua7qgwPfd6yD7MIez6vk"
+        }
+    });
+    const json = await res.json()
+
+    return {
+        props: {
+            HLSURLList: json
+        }
+    }
+}
+
+export default Videos;
