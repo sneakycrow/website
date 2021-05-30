b951a38dd2bf60130b987fd155cdd7f05ee44bd8
Zachary Sohovich <zach@sneakycrow.dev>
2021-05-29 17:11:37 -0700

    moved req to client side

--- pages/videos.js
+++ pages/videos.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import dynamic from "next/dynamic";
 import Intro from "../components/intro";
 
@@ -9,34 +9,28 @@ const Player = dynamic(
     {ssr: false}
 );
 
-const Videos = ({HLSURLList = []}) => {
+const Videos = () => {
+    const [videoURLList, setVideoURLList] = useState([]);
+    useEffect(async () => {
+        const res = await fetch("https://qln1awnny3.execute-api.us-east-1.amazonaws.com/default/dionysus-consume", {
+            headers: {
+                "X-Api-Key": "J3ORQvoTHZ9iuPDAaPxAua7qgwPfd6yD7MIez6vk"
+            }
+        });
+        const json = await res.json()
+        setVideoURLList(json)
+    }, [])
     return (
         <div className="flex flex-col items-center">
             <Intro subtitle={"videos and clips"}/>
 
-            {HLSURLList.map((url, index) => (
+            {videoURLList.map((url, index) => (
                 <div className="w-1/2 my-20" key={`hls-${index}`}>
                     <Player url={url}/>
                 </div>
             ))}
         </div>
     )
-}
-
-
-export async function getServerSideProps(context) {
-    const res = await fetch("https://qln1awnny3.execute-api.us-east-1.amazonaws.com/default/dionysus-consume", {
-        headers: {
-            "X-Api-Key": "J3ORQvoTHZ9iuPDAaPxAua7qgwPfd6yD7MIez6vk"
-        }
-    });
-    const json = await res.json()
-
-    return {
-        props: {
-            HLSURLList: json
-        }
-    }
 }
 
 export default Videos;
