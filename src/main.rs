87045cebeec6ab7869ef048cc164bb5079e0ac0a
sneakycrow <zach@sneakycrow.dev>
2022-11-15 13:33:37 -0800

    add initial logic for parsing index_page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
+use crate::website::Website;
+
+mod website;
+
+static OUTPUT_DIR: &str = "_out";
+
+fn main() -> Result<(), std::io::Error> {
+    // Initialize by making sure all output directories are ready
+    fs::create_dir_all(OUTPUT_DIR)?;
+    let website = Website::default()?;
+    let html: Vec<(String, String)> = website.generate_html();
+    for (title, html) in html {
+        let mut index_file = File::create(format!("{}/{}", OUTPUT_DIR, title))?;
+        index_file.write_all(html.as_bytes())?;
+    }
+
+    Ok(())
 }
