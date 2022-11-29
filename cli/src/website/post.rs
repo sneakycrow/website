808d4fce0d0eeed851760368795c87a146d9b39c
sneakycrow <zach@sneakycrow.dev>
2022-11-29 14:19:05 -0800

    add syntax highlighting to blog posts

--- cli/src/website/post.rs
+++ cli/src/website/post.rs
@@ -1,7 +1,10 @@
 use std::path::{Path, PathBuf};
 
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, CodeBlockKind, CowStr, Event, Options, Parser, Tag};
 use serde::{Deserialize, Serialize};
+use syntect::highlighting::ThemeSet;
+use syntect::html::highlighted_html_for_string;
+use syntect::parsing::SyntaxSet;
 
 #[derive(Debug, PartialEq, Deserialize)]
 struct YamlHeader {
@@ -41,13 +44,66 @@ impl Post {
         let YamlHeader { title } =
             serde_yaml::from_str(yaml).expect("[YAML ERROR] Could not parse yaml header");
 
+        // parse markdown
         let markdown_content = &contents[end_of_yaml..];
         let mut options = Options::empty();
         options.insert(Options::ENABLE_STRIKETHROUGH);
         let parser = Parser::new_ext(&markdown_content, options);
 
         let mut markdown = String::new();
-        html::push_html(&mut markdown, parser);
+        let mut modified_parser = Vec::new();
+        let mut to_highlight = String::new();
+
+        // Setup for syntect to highlight (specifically) Rust code
+        let ss = SyntaxSet::load_defaults_newlines();
+
+        let ts = ThemeSet::load_from_folder("_themes")
+            .expect("[THEME LOAD ERROR] Cannot load _themes folder");
+        let theme = &ts.themes["dracula"];
+
+        // And track a little bit of state
+        let mut in_code_block = false;
+
+        for event in parser {
+            match event {
+                Event::Start(Tag::CodeBlock(_)) => {
+                    // In actual use you'd probably want to keep track of what language this code is
+                    in_code_block = true;
+                }
+                Event::End(Tag::CodeBlock(kind)) => {
+                    if in_code_block {
+                        let syntax = match kind {
+                            CodeBlockKind::Fenced(lang) => match ss.find_syntax_by_token(&lang) {
+                                Some(val) => val,
+                                None => ss.find_syntax_plain_text(),
+                            },
+                            _ => ss.find_syntax_plain_text(),
+                        };
+                        // Format the whole multi-line code block as HTML all at once
+                        let html = highlighted_html_for_string(&to_highlight, &ss, &syntax, &theme)
+                            .expect("[SYNTAX HIGHLIGHT ERROR] Could not highlight html for string");
+                        // And put it into the vector
+                        modified_parser.push(Event::Html(CowStr::from(html)));
+                        to_highlight = String::new();
+                        in_code_block = false;
+                    }
+                }
+                Event::Text(t) => {
+                    if in_code_block {
+                        // If we're in a code block, build up the string of text
+                        to_highlight.push_str(&t);
+                    } else {
+                        modified_parser.push(Event::Text(t))
+                    }
+                }
+                e => {
+                    modified_parser.push(e);
+                }
+            }
+        }
+
+        // Now we send this new vector of events off to be transformed into HTML
+        html::push_html(&mut markdown, modified_parser.into_iter());
 
         // finally, the url.
         let mut url = PathBuf::from(&*filename);
