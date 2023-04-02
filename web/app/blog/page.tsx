780ebad1ed6d93c82d9b9cbdceff9f04835b076a
sneakycrow <zach@sneakycrow.dev>
2023-04-02 13:58:08 -0700

    added example placeholder request for blog data

--- web/app/blog/page.tsx
+++ web/app/blog/page.tsx
@@ -1,5 +1,27 @@
-const Blog = () => {
-  return <p>blog</p>;
+import React from "react";
+import Header from "@/app/Header";
+
+const getData = async (): Promise<BlogPost[]> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return await res.json();
+};
+
+type BlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+const Blog = async () => {
+  const data = await getData();
+
+  return (
+    <>
+      <Header />
+      <div></div>
+    </>
+  );
 };
 
 export default Blog;
