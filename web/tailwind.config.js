8f459fd1384e379fc0d9b74f62a42bb5cc17697b
sneakycrow <zach@sneakycrow.dev>
2023-04-01 23:09:27 -0700

    added font and color overrides to tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        black: "#1f1f1f",
+        green: {
+          550: "#0ba750",
+        },
+      },
+      lineHeight: {
+        12: "3rem",
+        13: "4.4rem",
+        15: "5rem",
+      },
+      fontSize: {
+        "3xl": "4.8rem",
+        "2xl": "4.2rem",
+        base: "2rem",
+      },
+    },
   },
   plugins: [],
 };
