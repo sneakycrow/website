44a1c26d117c6c8fadf0420f4a1012523f1043c3
sneakycrow <zach@sneakycrow.dev>
2023-04-02 21:24:32 -0700

    added wrapper around post list

--- web/app/blog/PostList.tsx
+++ web/app/blog/PostList.tsx
@@ -3,10 +3,12 @@ import { getPosts, ShortPost } from "@/components/Post";
 const PostList = async () => {
   const posts = await getPosts();
   return (
-    <div className="flex flex-col w-full bg-white">
-      {posts.map((post) => (
-        <ShortPost key={post.id} short post={post} />
-      ))}
+    <div className="flex flex-col w-full bg-white p-6 col-span-2">
+      <div className="w-2/3 mx-auto">
+        {posts.map((post) => (
+          <ShortPost key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
