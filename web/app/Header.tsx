5b2778adaf39f5c9d75790bb3afbf0c263708dae
sneakycrow <zach@sneakycrow.dev>
2023-04-02 21:24:02 -0700

    added dynamic title/subtitle for header

--- web/app/Header.tsx
+++ web/app/Header.tsx
@@ -1,9 +1,14 @@
 import Hero from "@/components/Hero";
 import Navigation from "@/components/Navigation";
 
-const Header = () => (
+interface HeaderProps {
+  title?: string;
+  subtitle?: string;
+}
+
+const Header = (props: HeaderProps) => (
   <header className="w-full grid grid-flow-row xl:grid-flow-col lg:col-span-2">
-    <Hero />
+    <Hero title={props.title} subtitle={props.subtitle} />
     <Navigation />
   </header>
 );
