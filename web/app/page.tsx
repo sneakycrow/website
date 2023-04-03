85bc8cf5cc41c3f55f21f8ccc7529b087a8261dc
sneakycrow <zach@sneakycrow.dev>
2023-04-02 21:24:09 -0700

    added MOTD to index page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,17 @@
 import Header from "@/app/Header";
 import AboutMe from "@/app/AboutMe";
+import React from "react";
+import MOTD from "@/components/MOTD";
 
 const Home = async () => {
   return (
     <main className="grid lg:grid-cols-2 grid-cols-1 gap-4 grid-flow-row bg-black p-4 min-h-screen">
       <Header />
       <div className="bg-white w-full">
-        <p>testing</p>
+        <MOTD
+          message="To be able to understand an idea without accepting it is the mark of an educated mind"
+          author="Aristotle"
+        />
       </div>
       <div className="bg-white">
         <AboutMe />
