3c74bd7a4d48425353108e72f027144cd1d28287
sneakycrow <zach@sneakycrow.dev>
2023-04-03 22:56:28 -0700

    add posts title to posts page

--- web/app/blog/PostList.tsx
+++ web/app/blog/PostList.tsx
@@ -3,8 +3,11 @@ import { getPosts, ShortPost } from "@/components/Post";
 const PostList = async () => {
   const posts = await getPosts();
   return (
-    <div className="flex flex-col w-full bg-white p-6 col-span-2">
-      <div className="w-2/3 mx-auto">
+    <div className="flex flex-col w-full bg-white p-6 col-span-2 lg:items-center">
+      <h3 className="text-xl lg:w-2/3 mb-4 font-bold after:w-2 after:bg-green-550 after:h-full after:absolute relative after:-skew-x-12 after:ml-2">
+        Posts
+      </h3>
+      <div className="lg:w-2/3">
         {posts.map((post) => (
           <ShortPost key={post.id} post={post} />
         ))}
