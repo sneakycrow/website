3d99e9083d162844e52e8480568abb16572ff24c
sneakycrow <zach@sneakycrow.dev>
2023-04-05 21:40:11 -0700

    Move ReactMarkdown to client side component

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import { getPostBySlug } from "@/components/Post";
-import ReactMarkdown from "react-markdown";
+import Markdown from "@/components/Markdown";
 
 interface BlogPageProps {
   params: {
@@ -16,8 +16,8 @@ const BlogPage = async (props: BlogPageProps) => {
       <header className="col-span-2">
         <Hero title={data.title} subtitle="" />
       </header>
-      <article className="min-h-[75vh] bg-white col-span-2">
-        <ReactMarkdown>{data.body}</ReactMarkdown>
+      <article className="min-h-[75vh] bg-white col-span-2 p-6 leading-loose">
+        <Markdown>{data.body}</Markdown>
       </article>
     </>
   );
