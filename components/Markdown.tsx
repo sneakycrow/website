1f896fec725a4dba968a7246f6d2b35432389b7c
sneakycrow <zach@sneakycrow.dev>
2023-04-08 23:08:20 -0700

    added additional element stylings to markdown

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -14,15 +14,24 @@ const Markdown = (props: MarkdownProps) => {
     <ReactMarkdown
       className="max-w-[800px]"
       components={{
-        p: ({ ...props }) => <p {...props} className="text-lg" />,
+        p: ({ ...props }) => <p {...props} className="text-lg leading-1" />,
+        a: ({ ...props }) => (
+          <a {...props} className="text-green-550 font-bold" />
+        ),
+        h1: ({ ...props }) => (
+          <h1 {...props} className="text-xl font-bold my-4" />
+        ),
         h2: ({ ...props }) => (
-          <h2 {...props} className="text-xl leading-loose" />
+          <h2 {...props} className="text-xl font-bold my-4" />
         ),
         ol: ({ ...props }) => (
           <ol {...props} className="list-disc list-inside" />
         ),
         ul: ({ ...props }) => (
           <ul {...props} className="list-disc list-inside" />
+        ),
+        img: ({ ...props }) => (
+          <img {...props} className="max-w-[90%] mx-auto shadow-lg my-4" />
         ),
         code: ({ node, inline, className, children, ...props }) => {
           const match = /language-(\w+)/.exec(className || "");
