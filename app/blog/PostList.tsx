8fc0982f9915d6f327110a7bc6076af8d4cae665
sneakycrow <zach@sneakycrow.dev>
2023-04-08 23:08:27 -0700

    added dividers to post list

--- app/blog/PostList.tsx
+++ app/blog/PostList.tsx
@@ -7,7 +7,7 @@ const PostList = async () => {
       <h3 className="text-xl lg:w-2/3 mb-4 font-bold after:w-2 after:bg-green-550 after:h-full after:absolute relative after:-skew-x-12 after:ml-2">
         Posts
       </h3>
-      <div className="lg:w-2/3">
+      <div className="lg:w-2/3 divide-y-2">
         {posts.map((post) => (
           <ShortPost key={post.id} post={post} />
         ))}
