a8be92ad335347ae556590ef09b436ad177814b6
sneakycrow <zach@sneakycrow.dev>
2023-04-25 00:07:42 -0700

    move open graph config to its own file

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,31 +1,16 @@
 import React from "react";
+import { openGraph } from "@/app/openGraph";
 
 export const metadata = {
   title: "award-winning blog by sneakycrow",
   description: "the thoughts of a mad scientist",
-  openGraph: {
-    title: "sneakycrow",
-    description:
-      "open source software, web development, and more by Zachary Sohovich",
-    url: "https://sneakycrow.dev",
-    images: [
-      {
-        url: "https://sneakycrow.dev/logo_v2.svg",
-        width: 200,
-        height: 200,
-      },
-    ],
-  },
+  openGraph,
 };
 
-export default function BlogLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <main className="grid gap-4 grid-flow-row bg-black p-4 min-h-screen">
-      {children}
-    </main>
-  );
-}
+const BlogLayout = ({ children }: { children: React.ReactNode }) => (
+  <main className="grid gap-4 grid-flow-row bg-black p-4 min-h-screen">
+    {children}
+  </main>
+);
+
+export default BlogLayout;
