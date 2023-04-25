2631c4110f995555d07e21e6ddcaa0b515cc3907
sneakycrow <zach@sneakycrow.dev>
2023-04-24 22:15:09 -0700

    re-stylize and update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,33 @@
-interface FooterProps {}
+import Link from "next/link";
 
-const Footer = (props: FooterProps) => {
+const subnavLinks: { url: string; copy: string; rel?: string }[] = [
+  {
+    copy: "github",
+    url: "https://github.com/sneakycrow",
+  },
+  {
+    copy: "resume",
+    url: "/resume.pdf",
+  },
+  {
+    copy: "mastadon",
+    url: "https://cybre.gg/@ctrlsquid",
+    rel: "me",
+  },
+];
+
+const Footer = () => {
   return (
-    <footer className="bg-gray-100 p-4 w-full text-center">
-      <h3 className="text-3xl">Want more?</h3>
-      Find me on {` `}
-      <a className="text-green-550" href="https://cybre.gg/@ctrlsquid">
-        my mastadon, cybre.gg
-      </a>
+    <footer className="bg-gray-100 p-4 w-full flex flex-col justify-evenly xl:flex-row items-center">
+      {subnavLinks.map((link) => (
+        <Link
+          href={link.url}
+          {...link}
+          className="text-2xl font-bold text-green-550 opacity-75 hover:opacity-100 transition-opacity"
+        >
+          {link.copy}
+        </Link>
+      ))}
     </footer>
   );
 };
