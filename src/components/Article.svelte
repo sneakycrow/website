c3e9f1a57b2670c7a953caec9b55fdbad4809e14
sneakycrow <zach@sneakycrow.dev>
2023-07-16 11:02:41 -0700

    replace sveltemakrdown with markdownrenderer

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
-  import SvelteMarkdown from "svelte-markdown";
-  import Code from "./post/Code.svelte";
-  import Paragraph from "./post/Paragraph.svelte";
-  import Link from "./post/Link.svelte";
-  import Heading from "./post/Heading.svelte";
-  import List from "./post/List.svelte";
   import type { Post } from "$lib/posts";
+  import MarkdownRenderer from "./MarkdownRenderer.svelte";
 
   export let post: Post;
 </script>
 
 <div class="bg-white p-6 flex justify-center">
   <article class="space-y-8 w-full lg:w-3/4 max-w-[1000px]">
-    <SvelteMarkdown
-      renderers={{
-        code: Code,
-        paragraph: Paragraph,
-        link: Link,
-        heading: Heading,
-        list: List
-      }}
-      source={post.body} />
+    <MarkdownRenderer source={post.body} />
   </article>
 </div>
