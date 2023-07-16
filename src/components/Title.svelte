31969d4d3a1a2d78f9bd50f2f4ca03e1a5b242f1
sneakycrow <zach@sneakycrow.dev>
2023-07-16 11:02:32 -0700

    enable size

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,3 +1,8 @@
-<h3 class="text-xl font-bold underline decoration-4 decoration-green-550">
+<script lang="ts">
+  export let size: "sm" | "base" | "xl" = "xl";
+
+</script>
+
+<h3 class={`text-${size} font-bold underline decoration-4 decoration-green-550`}>
   <slot />
 </h3>
