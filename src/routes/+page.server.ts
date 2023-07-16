0179e472ef0af1ec4b5f9cd89eedcb358f5b70ad
sneakycrow <zach@sneakycrow.dev>
2023-07-16 11:03:02 -0700

    add MOTD to homepage

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageServerLoad } from "./$types";
+import { getMastodonStatus } from "$lib/motd";
+
+export const load = (async ({ params }) => {
+  const statuses = await getMastodonStatus();
+  const latestStatus = statuses[0];
+  console.log(latestStatus);
+  return {
+    motd: {
+      content: latestStatus.content,
+      url: latestStatus.url
+    }
+  };
+}) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,16 @@
   import Header from "../components/Header.svelte";
   import BannerLinks from "../components/BannerLinks.svelte";
   import AboutMe from "../components/AboutMe.svelte";
+  import type { PageServerData } from "./$types";
+  import MOTD from "../components/MOTD.svelte";
+
+  export let data: PageServerData;
 </script>
 
 <main class="grid gap-4 grid-flow-row bg-black p-4 min-h-screen max-w-full">
   <Header subtitle="software wizard" title="Sneaky Crow" />
   <BannerLinks />
-
+  <MOTD content={data.motd.content} source={data.motd.source} />
   <div class="bg-white w-full flex flex-col justify-center items-center">
     <AboutMe />
   </div>
