6e30b2e29346973a650759fc226229b78b0d0cb6
sneakycrow <zach@sneakycrow.dev>
2023-07-16 11:02:27 -0700

    add markdown renderer

---
+++ src/components/MarkdownRenderer.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Code from "./post/Code.svelte";
+  import Paragraph from "./post/Paragraph.svelte";
+  import Link from "./post/Link.svelte";
+  import Heading from "./post/Heading.svelte";
+  import List from "./post/List.svelte";
+  import SvelteMarkdown from "svelte-markdown";
+
+  export let source;
+</script>
+
+<SvelteMarkdown
+  renderers={{
+        code: Code,
+        paragraph: Paragraph,
+        link: Link,
+        heading: Heading,
+        list: List
+      }}
+  source={source}
+/>
