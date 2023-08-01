93c4dc8fbce0586262b5fa5adc9a04f41042fde1
sneakycrow <zach@sneakycrow.dev>
2023-07-31 23:29:33 -0700

    add featured post

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,13 @@
 import type { PageServerLoad } from "./$types";
 import { getMastodonStatus } from "$lib/motd";
+import { getAllPosts } from "$lib/posts";
 
 export const prerender = true;
 
 export const load = (async ({ params }) => {
   const statuses = await getMastodonStatus();
   const latestStatus = statuses[0];
+  const [mostRecentPost] = await getAllPosts();
   return {
     motd: {
       content: latestStatus.content,
@@ -14,6 +16,10 @@ export const load = (async ({ params }) => {
         latestStatus.media_attachments.find(
           (img: { type: string; url: string }) => img.type === "image"
         )?.url ?? undefined
+    },
+    featuredPost: {
+      ...mostRecentPost,
+      date: new Date(mostRecentPost.date).toLocaleDateString()
     }
   };
 }) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,39 @@
   import AboutMe from "$lib/components/AboutMe.svelte";
   import MOTD from "$lib/components/MOTD.svelte";
   import type { PageServerData } from "./$types";
+  import Title from "$lib/components/Title.svelte";
 
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+
+  let ready = false;
   export let data: PageServerData;
+  onMount(() => {
+    ready = true;
+  });
 </script>
 
 <main class="grid gap-4 grid-flow-row bg-black p-4 min-h-screen max-w-full">
   <Header subtitle="software wizard" title="Sneaky Crow" />
   <BannerLinks />
-  <MOTD content={data.motd.content} image={data.motd.image} source={data.motd.url} />
+  <div class="flex divide-x-[16px] divide-black bg-white">
+    <div class="w-1/2">
+      <MOTD content={data.motd.content} image={data.motd.image} source={data.motd.url} />
+    </div>
+    <div class="w-1/2 p-4 flex flex-col h-full justify-between" in:fade={{ delay: 250, duration: 300 }}>
+      {#if ready}
+        <Title size="sm">Featured Post</Title>
+        <div class="self-center text-lg">
+          <a href={`/blog/${data.featuredPost.slug}`}>{data.featuredPost.title}</a>
+        </div>
+      {/if}
+      <p
+        class="text-xs text-gray-400 italic self-end hover:cursor-pointer flex items-center justify-center">
+        {data.featuredPost.date}
+      </p>
+    </div>
+  </div>
   <div class="bg-white w-full flex flex-col justify-center items-center lg:p-0 p-4">
     <AboutMe />
   </div>
