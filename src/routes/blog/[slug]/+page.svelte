6d4078137003d41008dded5d37fb4916f08a3626
Zachary Corvidae <106559028+ctrlsquid@users.noreply.github.com>
2023-08-10 23:59:17 -0700

    Fix breaking article width on mobile (#187)

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -9,16 +9,18 @@
 </script>
 
 <main class="flex flex-col gap-4 bg-black p-4">
-  <Header subtitle="" title={data.post.title} />
-  <Article post={data.post} />
-  {#if data.series?.length ?? [] > 0}
+  <Header subtitle="" title={data.post?.title ?? ""} />
+  {#if data.post}
+    <Article post={data.post} />
+  {/if}
+  {#if data.series?.length > 0}
     <div class="w-full bg-white flex flex-col items-center justify-center">
       <div class="p-4 lg:p-0 lg:py-2 max-w-[1000px] w-full">
         <Title>Series</Title>
         <ol
           class="list-decimal list-inside grid grid-flow-row items-center space-y-2 text-lg">
           {#each data.series as post}
-            <li class={`${post.slug === data.post.slug ? "text-green-550 opacity-60" : "text-green-550"}`}>
+            <li class={`${post.slug === data.post?.slug ? "text-green-550 opacity-60" : "text-green-550"}`}>
               <a href={`/blog/${post.slug}`}>{post.title}</a>
             </li>
           {/each}
