a9aacc952e1906c889ec6b7a16f660bc1e815823
Zachary Corvidae <106559028+ctrlsquid@users.noreply.github.com>
2023-11-17 03:50:33 -0800

    Add Sentry configuration (#211)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,15 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 import { auth } from "$lib/server/lucia";
 import type { Handle } from "@sveltejs/kit";
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+  dsn: "https://0e272c5b37f4fc8bedc85ee8716183f1@o4506240903806976.ingest.sentry.io/4506240904003584",
+  tracesSampleRate: 1
+});
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
   event.locals.auth = auth.handleRequest(event);
   return resolve(event);
-};
+});
+export const handleError = Sentry.handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [sveltekit(), wasm(), topLevelAwait()]
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: "sneakycrow",
+        project: "sneakycrow-dev"
+      }
+    }),
+    sveltekit(),
+    wasm(),
+    topLevelAwait()
+  ]
 });
