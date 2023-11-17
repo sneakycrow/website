aae4601958b1e6e7dc39a95ca8f42ade33f1e3e9
sneakycrow <zach@sneakycrow.dev>
2023-11-17 00:52:46 -0800

    reset migrations

--- prisma/migrations/20231117084159_init/migration.sql
+++ prisma/migrations/20231117084159_init/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User"
 (
     "id"       TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "avatar"   TEXT,
+    "email"    TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -33,6 +35,9 @@ CREATE UNIQUE INDEX "User_id_key" ON "User" ("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User" ("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User" ("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_id_key" ON "Session" ("id");
