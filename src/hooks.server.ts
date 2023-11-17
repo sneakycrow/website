a8aa03145623bbbba7885d9b94cfce21700bde04
sneakycrow <zach@sneakycrow.dev>
2023-11-17 00:53:13 -0800

    uncomment hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,7 @@
-// export const handle: Handle = async ({ event, resolve }) => {
-//   // we can pass `event` because we used the SvelteKit middleware
-//   event.locals.auth = auth.handleRequest(event);
-//   return resolve(event);
-// };
+import { auth } from "$lib/server/lucia";
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.auth = auth.handleRequest(event);
+  return resolve(event);
+};
