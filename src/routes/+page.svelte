541ec1f005e7893e3a16f2e5a41f0c0a1f900c87
sneakycrow <zach@sneakycrow.dev>
2023-11-17 02:03:34 -0800

    add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
   import BannerLinks from "$lib/components/BannerLinks.svelte";
   import AboutMe from "$lib/components/AboutMe.svelte";
   import PostList from "$lib/components/PostList.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import { enhance } from "$app/forms";
 
-  export let user: { username: string; avatar: string; } | undefined;
   export let data: PageServerData;
 </script>
 
@@ -13,7 +14,24 @@
   <Header subtitle="software wizard" title="Sneaky Crow" />
   <BannerLinks />
   {#if data.posts}
-    <PostList posts={data.posts} title="Recent Posts" />
+    <PostList posts={data.posts} title="Recent Posts" cols="grid-cols-2" />
   {/if}
   <AboutMe />
+  <Footer>
+    <div class="flex flex-col space-y-2" slot="left">
+      <span class="text-sm">© {new Date().getFullYear()} Sneaky Crow, LLC</span>
+      <span class="text-sm">All Rights Reserved</span>
+    </div>
+    <div class="flex flex-col space-y-2" slot="right">
+      {#if data.username}
+        <p class="text-sm">Logged in as <span class="font-bold">{data.username}</span></p>
+        <form action="/?/logout" method="post" use:enhance>
+          <input class="w-full block text-left cursor-pointer text-green-550 font-bold" type="submit"
+                 value="Log Out" />
+        </form>
+      {:else}
+        <a class="w-full block text-left cursor-pointer hover:text-cobrashare-green-6" href="/login">Log In</a>
+      {/if}
+    </div>
+  </Footer>
 </main>
