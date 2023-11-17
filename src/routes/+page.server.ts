cc542355d39c6bd85f01e06a99390798de57a7d0
sneakycrow <zach@sneakycrow.dev>
2023-11-17 00:54:31 -0800

    re-add posts to index page load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,24 @@
 import { getFeaturedPosts, type Post } from "$lib/posts";
+import type { PageServerLoad } from "./$types";
 
-export const load = async (): Promise<{ posts: Post[] }> => {
-  const posts = await getFeaturedPosts();
-  const postsWithPrefix: Post[] = posts.map((p) => {
+export const load: PageServerLoad = async ({ locals }) => {
+  try {
+    const session = await locals.auth.validate();
+
+    const posts = await getFeaturedPosts();
+    const postsWithPrefix: Post[] = posts.map((p) => {
+      return {
+        ...p,
+        slug: `/blog/${p.slug}`
+      };
+    });
+    if (!session) return { posts: postsWithPrefix };
     return {
-      ...p,
-      slug: `/blog/${p.slug}`
+      posts: postsWithPrefix,
+      username: session.username
     };
-  });
-  return {
-    posts: postsWithPrefix
-  };
+  } catch (e) {
+    console.error(`Could not load page on server ${e}`);
+    return { posts: [] };
+  }
 };
