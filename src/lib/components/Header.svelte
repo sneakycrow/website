ec2f7d76ba3c963ed347656c2c5f28afcbefb7fe
sneakycrow <zach@sneakycrow.dev>
2024-05-01 11:59:39 -0700

    fix logo on mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,7 +10,7 @@
   class={`w-full grid grid-flow-dense grid-cols-3 lg:gap-10 lg:grid-rows-3 items-center justify-between lg:max-h-[300px] ${$$restProps.class}`}
 >
   <Logo
-    class="row-start-1 py-4 lg:row-span-2 col-start-1 lg:col-span-1 col-span-3 w-full lg:w-[200px] place-self-center justify-self-start"
+    class="row-start-1 py-4 lg:row-span-2 col-start-1 lg:col-span-1 col-span-3 mx-auto lg:ml-0 w-[100px] lg:w-[200px] place-self-center justify-self-start"
   />
   <Navigation class="w-full lg:row-start-1 row-start-2 col-start-2 col-span-2" />
   <section
