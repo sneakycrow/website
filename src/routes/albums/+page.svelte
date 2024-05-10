09e528db32bd1b1a89c690e6b19e1f8fcfaba861
sneakycrow <zach@sneakycrow.dev>
2024-05-09 18:15:41 -0700

    add styles to album page

--- src/routes/albums/+page.svelte
+++ src/routes/albums/+page.svelte
@@ -11,19 +11,24 @@
   </div>
 {/if}
 
-<section class="lg:col-span-6 row-start-2 row-span-2 grid grid-cols-3 gap-10">
-  <p class="col-span-3 text-center text-lg font-semibold">
-    A collection of albums provided by Spotify
+<section class="lg:col-span-6 row-start-2 row-span-2 grid lg:grid-cols-3 gap-10">
+  <p class="lg:col-span-3 text-center text-lg font-semibold">
+    All the albums I remembered to save on Spotify
   </p>
   {#each albums as albumData}
     <div class="space-y-2 flex flex-col items-center justify-center">
-      <img
-        src={albumData.album.images[1].url}
-        alt={albumData.album.name}
-        width={albumData.album.images[1].width}
-        height={albumData.album.images[1].height}
+      <a
+        href={albumData.album.external_urls.spotify}
+        target="_blank"
         class="shadow-lg rounded-lg p-4"
-      />
+      >
+        <img
+          src={albumData.album.images[1].url}
+          alt={albumData.album.name}
+          width={albumData.album.images[1].width}
+          height={albumData.album.images[1].height}
+        />
+      </a>
       <h3 class="font-bold">{albumData.album.name}</h3>
       <p class="text-sm">{albumData.album.artists[0].name}</p>
     </div>
