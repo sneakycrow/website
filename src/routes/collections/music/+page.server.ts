7e6c44410b0f982dddb0d7816e3bebb1dffdfa72
sneakycrow <zach@sneakycrow.dev>
2024-05-10 12:19:32 -0700

    dont catch error for music route

--- src/routes/collections/music/+page.server.ts
+++ src/routes/collections/music/+page.server.ts
@@ -1,20 +1,9 @@
 import type { PageServerLoad } from "./$types";
 import { getSneakyCrowTopArtists } from "$lib/spotify";
 
-const ERROR_MSG =
-  "Zach failed to connect this website to Spotify, blame him for this error and try again later.";
 export const load: PageServerLoad = async () => {
-  try {
-    const artistData = await getSneakyCrowTopArtists();
-    return {
-      artists: artistData
-    };
-  } catch (e) {
-    console.error(`Could not load album page ${e}`);
-    return {
-      error: {
-        message: ERROR_MSG
-      }
-    };
-  }
+  const artistData = await getSneakyCrowTopArtists();
+  return {
+    artists: artistData
+  };
 };
