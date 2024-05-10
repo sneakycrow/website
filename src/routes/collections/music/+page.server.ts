ccbc33effeaebce08b58193dac59fe7c6d6b1609
sneakycrow <zach@sneakycrow.dev>
2024-05-10 13:32:13 -0700

    comment out bug code

--- src/routes/collections/music/+page.server.ts
+++ src/routes/collections/music/+page.server.ts
@@ -1,22 +1,23 @@
 import type { PageServerLoad } from "./$types";
 import { getSneakyCrowTopArtists } from "$lib/spotify";
-import { getExpirationByDays, getFromRedis, saveToRedis } from "$lib/server/redis";
+// import { getExpirationByDays, getFromRedis, saveToRedis } from "$lib/server/redis";
 
 export const load: PageServerLoad = async () => {
-  const cachedArtistData = await getFromRedis("sneaky-crow-top-artists");
-  if (cachedArtistData) {
-    return {
-      artists: JSON.parse(cachedArtistData)
-    };
-  }
+  // BUG: In production this causes the app to crash
+  // const cachedArtistData = await getFromRedis("sneaky-crow-top-artists");
+  // if (cachedArtistData) {
+  //   return {
+  //     artists: JSON.parse(cachedArtistData)
+  //   };
+  // }
   const artistData = await getSneakyCrowTopArtists();
-  if (artistData) {
-    await saveToRedis(
-      "sneaky-crow-top-artists",
-      JSON.stringify(artistData),
-      getExpirationByDays(7)
-    );
-  }
+  // if (artistData) {
+  //   await saveToRedis(
+  //     "sneaky-crow-top-artists",
+  //     JSON.stringify(artistData),
+  //     getExpirationByDays(7)
+  //   );
+  // }
   return {
     artists: artistData
   };
