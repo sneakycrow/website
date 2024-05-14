6dc4a2c67ea08ef3c7b3abff848d00eb203fdf9d
Zachary Corvidae <zach@sneakycrow.dev>
2024-05-14 13:16:42 -0700

    Add art and games to collections, tmp disabled (#256)

--- src/routes/collections/+page.svelte
+++ src/routes/collections/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-
+  type Link = {
+    copy: string;
+    url: string;
+    description: string;
+    class: string;
+    iconColor: string;
+    icon: string;
+    comingSoon?: boolean;
+  };
   const links = [
     {
       copy: "articles",
@@ -17,6 +25,24 @@
       class: "hover:text-green-500",
       iconColor: "text-green-500 opacity-75",
       icon: "material-symbols:album"
+    },
+    {
+      copy: "games",
+      url: "/collections/games",
+      description: "where my dreams become virtual reality (uh oh)",
+      class: "hover:text-blue-500",
+      iconColor: "text-blue-500 opacity-75",
+      icon: "f7:gamecontroller-fill",
+      comingSoon: true
+    },
+    {
+      copy: "art",
+      url: "/collections/art",
+      description: "I like to make pretty things sometimes",
+      class: "hover:text-red-500",
+      iconColor: "text-red-500 opacity-75",
+      icon: "tabler:palette",
+      comingSoon: true
     }
   ];
   const ICON_SIZE = 100;
@@ -27,14 +53,21 @@
 </svelte:head>
 
 {#each links as link}
-  <div class="lg:col-span-3 text-center">
+  <div class={`lg:col-span-3 text-center ${link.comingSoon ? "grayscale" : ""}`}>
     <a
-      href={link.url}
-      class={`text-gray-900 lg:text-5xl font-bold uppercase flex flex-nowrap space-x-2 items-center justify-center ${link.class}`}
+      href={link.comingSoon ? "#" : link.url}
+      class={`${
+        link.comingSoon ? "text-gray-500" : "text-gray-900"
+      } text-5xl font-bold uppercase flex flex-nowrap space-x-2 items-center justify-center ${
+        link.class
+      }`}
     >
       <Icon icon={link.icon} height={ICON_SIZE} width={ICON_SIZE} class={link.iconColor} />
       {link.copy}
     </a>
     <p class="text-sm text-gray-500">{link.description}</p>
+    {#if link.comingSoon}
+      <p class="text-lg uppercase text-gray-500 font-semibold my-4">Coming Soon</p>
+    {/if}
   </div>
 {/each}
