b7123a694e5c173a6c89579ab4cd9c6390a31907
sneakycrow <zach@sneakycrow.dev>
2024-05-15 09:12:39 -0700

    add source code link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Icon from "@iconify/svelte";
   import Avatar from "./AvatarWidget.svelte";
 
   type User = {
@@ -11,9 +12,16 @@
 <footer
   class={`py-4 flex flex-col space-y-10 lg:flex-row justify-between items-center ${$$restProps.class}`}
 >
-  <p>
+  <p class="flex flex-col">
     <span class="text-sm">© {new Date().getFullYear()} Sneaky Crow, LLC</span>
     <span class="text-sm">All Rights Reserved</span>
+    <a
+      class="text-gray-500 font-normal flex items-center space-x-2"
+      href="https://github.com/sneakycrow/website"
+    >
+      <span>source code for this site</span>
+      <Icon icon="akar-icons:github-fill" class="inline-block w-4 h-4" />
+    </a>
   </p>
   {#if user}
     <Avatar {user} />
