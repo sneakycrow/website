e1a1968c5b225e225e62e6db4c6f94e85f4c4996
sneakycrow <zach@sneakycrow.dev>
2024-05-28 16:32:03 -0700

    simplify collections page design

--- src/routes/collections/+page.svelte
+++ src/routes/collections/+page.svelte
@@ -8,104 +8,50 @@
     iconColor: string;
     icon: string;
     comingSoon?: boolean;
-    subPages?: {
-      copy: string;
-      url: string;
-      class?: string;
-    }[];
   };
   const links: Link[] = [
     {
-      copy: "literature",
+      copy: "writing",
       url: "/blog",
       description: "brain juice, mostly",
       class: "hover:text-yellow-500",
       iconColor: "text-yellow-500 opacity-75",
-      icon: "tabler:writing",
-      subPages: [
-        {
-          copy: "blog",
-          url: "/blog",
-          class: "border-yellow-500 border-b-4"
-        },
-        {
-          copy: "poetry",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        },
-        {
-          copy: "book reviews",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        }
-      ]
+      icon: "tabler:writing"
     },
     {
-      copy: "music",
+      copy: "listening",
       url: "/music",
       description: "totally radical tunes",
       class: "hover:text-green-500",
       iconColor: "text-green-500 opacity-75",
-      icon: "material-symbols:album",
-      subPages: [
-        {
-          copy: "made by me",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        },
-        {
-          copy: "listening to",
-          url: "/music",
-          class: "border-green-500 border-b-4"
-        }
-      ]
+      icon: "material-symbols:album"
     },
     {
-      copy: "games",
+      copy: "reading",
+      url: "/music",
+      description: "feeding my brain with the written word",
+      class: "hover:text-purple-500",
+      iconColor: "text-purple-500 opacity-75",
+      icon: "tabler:book",
+      comingSoon: true
+    },
+    {
+      copy: "playing",
       url: "/games",
       description: "where my dreams become virtual reality (uh oh)",
       class: "hover:text-blue-500",
       iconColor: "text-blue-500 opacity-75",
       icon: "f7:gamecontroller-fill",
-      comingSoon: true,
-      subPages: [
-        {
-          copy: "made by me",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        },
-        {
-          copy: "reviews",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        }
-      ]
+      comingSoon: true
     },
     {
-      copy: "art",
+      copy: "creating",
       url: "/art",
       description: "I like to make pretty things sometimes",
       class: "hover:text-red-500",
       iconColor: "text-red-500 opacity-75",
       icon: "tabler:palette",
-      comingSoon: true,
-      subPages: [
-        {
-          copy: "digital",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        },
-        {
-          copy: "generative",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        },
-        {
-          copy: "photography",
-          url: "#",
-          class: "opacity-25 hover:opacity-25 cursor-not-allowed"
-        }
-      ]
+      comingSoon: true
     }
   ];
   const ICON_SIZE = 100;
@@ -115,29 +61,24 @@
   <title>treasure trash</title>
 </svelte:head>
 
-{#each links as link}
-  <div class={`lg:col-span-3 text-center gap-8 ${link.comingSoon ? "grayscale" : ""}`}>
-    <a
-      href={link.comingSoon ? "#" : link.url}
-      class={`${
-        link.comingSoon ? "text-gray-500" : "text-gray-900"
-      } text-xl lg:text-5xl font-bold uppercase flex flex-nowrap space-x-2 leading-none items-center justify-center ${
-        link.class
-      }`}
-    >
-      <Icon icon={link.icon} height={ICON_SIZE} width={ICON_SIZE} class={link.iconColor} />
-      {link.copy}
-    </a>
-    <p class="lg:text-sm text-gray-500 mb-4">{link.description}</p>
-    {#if link.subPages}
-      <div class="flex gap-4 text-center items-center justify-center">
-        {#each link.subPages as subpage}
-          <a href={subpage.url} class={`text-lg text-black ${subpage.class}`}>{subpage.copy}</a>
-        {/each}
-      </div>
-    {/if}
-    {#if link.comingSoon}
-      <p class="text-lg uppercase text-gray-500 font-semibold my-4">Coming Soon</p>
-    {/if}
-  </div>
-{/each}
+<section class="grid lg:grid-cols-2 lg:col-span-6 row-span-2 gap-8">
+  {#each links as link}
+    <div class={`text-center ${link.comingSoon ? "grayscale" : ""}`}>
+      <a
+        href={link.comingSoon ? "#" : link.url}
+        class={`${
+          link.comingSoon ? "text-gray-500" : "text-gray-900"
+        } text-xl lg:text-5xl font-bold uppercase flex flex-nowrap space-x-2 leading-none items-center justify-center ${
+          link.class
+        }`}
+      >
+        <Icon icon={link.icon} height={ICON_SIZE} width={ICON_SIZE} class={link.iconColor} />
+        {link.copy}
+      </a>
+      <p class="lg:text-sm text-gray-500 mb-4">{link.description}</p>
+      {#if link.comingSoon}
+        <p class="text-lg uppercase text-gray-500 font-semibold my-4">Coming Soon</p>
+      {/if}
+    </div>
+  {/each}
+</section>
