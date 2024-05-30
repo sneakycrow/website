67f7c2cd2b8087cbde254e3f3d7ae3e77dc21cf8
Zachary Corvidae <zach@sneakycrow.dev>
2024-05-30 11:32:57 -0700

    Add stream status to cache (#258)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,4 @@
+import { getExpirationByMinutes, getFromRedis, saveToRedis } from "$lib/server/redis";
 import { getStaticStream } from "$lib/twitch";
 import type { LayoutServerLoad } from "./$types";
 
@@ -15,9 +16,24 @@ export const load: LayoutServerLoad = async ({ locals, route }) => {
     // Check if I'm live on Twitch
     // Don't let this prevent the page from loading
     try {
-      const stream = await getStaticStream();
-      if (stream.live) {
-        pageMeta.isLive = true;
+      const cachedStream = await getFromRedis("static_stream_status");
+      if (cachedStream) {
+        // The value is expected to be a string boolean
+        pageMeta.isLive = cachedStream === "true";
+      } else {
+        const stream = await getStaticStream();
+        const isStreamLive = stream.live !== null;
+        if (isStreamLive) {
+          // If the stream is live, cache the result for an hour
+          // An hour is usually around the time I stream
+          pageMeta.isLive = true;
+        }
+        // Lastly, save the result to Redis for an hour
+        await saveToRedis(
+          "static_stream_status",
+          isStreamLive.toString(),
+          getExpirationByMinutes(60)
+        );
       }
     } catch (e) {
       console.error(`Could not get Twitch stream status: ${e}`);
