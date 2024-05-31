7dfe50049e0b1e19e39e41a53b1529a3291ab584
sneakycrow <zach@sneakycrow.dev>
2024-05-31 15:45:59 -0700

    add cache to top artists

--- src/routes/music/top-artists/+page.server.ts
+++ src/routes/music/top-artists/+page.server.ts
@@ -1,23 +1,23 @@
 import type { PageServerLoad } from "./$types";
 import { getSneakyCrowTopArtists } from "$lib/spotify";
-// import { getExpirationByDays, getFromRedis, saveToRedis } from "$lib/server/redis";
+import {
+  SPOTIFY_TOP_ARTISTS,
+  getFromRedis,
+  saveToRedis,
+  getExpirationByDays
+} from "$lib/server/redis";
 
 export const load: PageServerLoad = async () => {
-  // BUG: In production this causes the app to crash
-  // const cachedArtistData = await getFromRedis("sneaky-crow-top-artists");
-  // if (cachedArtistData) {
-  //   return {
-  //     artists: JSON.parse(cachedArtistData)
-  //   };
-  // }
+  const cachedArtistData = await getFromRedis(SPOTIFY_TOP_ARTISTS);
+  if (cachedArtistData) {
+    return {
+      artists: JSON.parse(cachedArtistData)
+    };
+  }
   const artistData = await getSneakyCrowTopArtists();
-  // if (artistData) {
-  //   await saveToRedis(
-  //     "sneaky-crow-top-artists",
-  //     JSON.stringify(artistData),
-  //     getExpirationByDays(7)
-  //   );
-  // }
+  if (artistData) {
+    await saveToRedis(SPOTIFY_TOP_ARTISTS, JSON.stringify(artistData), getExpirationByDays(7));
+  }
   return {
     artists: artistData
   };
