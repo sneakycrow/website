e207bd3c2e4f24e3cb614d5bc318db72b8cc0f61
sneakycrow <zach@sneakycrow.dev>
2024-06-25 22:18:54 -0700

    add get user with accounts and feed

--- src/lib/server/user/index.ts
+++ src/lib/server/user/index.ts
@@ -1,5 +1,32 @@
+import { Prisma } from "@prisma/client";
 import type { User } from "@prisma/client";
 import client from "$lib/server/db";
+
+// A user, including their accounts and feed items
+const userWithAccountsAndFeed = Prisma.validator<Prisma.UserDefaultArgs>()({
+  include: {
+    accounts: true,
+    feed: true
+  }
+});
+
+type UserWithAccountsAndFeed = Prisma.UserGetPayload<typeof userWithAccountsAndFeed>;
+
+// Get a user with their accounts and feed items by their ID
+// TODO: Probably optimize feed items to not get _all_ of them
+export const getUserWithAccountsAndFeedById = async (
+  id: string
+): Promise<UserWithAccountsAndFeed | null> => {
+  return client.user.findUnique({
+    where: {
+      id
+    },
+    include: {
+      accounts: true,
+      feed: true
+    }
+  });
+};
 
 export const getUserByUsername = async (username: string): Promise<User | null> => {
   return client.user.findUnique({
