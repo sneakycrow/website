5240c6a2f4a7d8a5912d3ee4997d7fa42c0c1e1f
sneakycrow <zach@sneakycrow.dev>
2024-06-25 18:36:25 -0700

    refactor language parser, add line numbers, add filename

--- src/lib/components/post/Code.svelte
+++ src/lib/components/post/Code.svelte
@@ -1,10 +1,77 @@
 <script lang="ts">
-  import { HighlightAuto } from "svelte-highlight";
-  import "svelte-highlight/styles/github-dark.css";
+  import Highlight, { LineNumbers, HighlightSvelte } from "svelte-highlight";
+  import {
+    rust,
+    plaintext,
+    typescript,
+    javascript,
+    json,
+    shell,
+    yaml
+  } from "svelte-highlight/languages";
+  // @ts-ignore - these types aren't support important to the project, so we ignore them
+  import gleam from "@gleam-lang/highlight.js-gleam";
+  import "svelte-highlight/styles/onedark.css";
 
   export let text: string;
+  // This is expected either to be a single string or a space-separated string, "rust filename.rs"
+  export let lang: string;
+  const parsedLang = lang.split(" ");
+  const language = parsedLang[0];
+  const filename = parsedLang[1] ?? "";
+  // import language based on lang prop
+  const getLanguage = () => {
+    switch (language) {
+      case "rust": {
+        return rust;
+      }
+      case "typescript": {
+        return typescript;
+      }
+      case "js":
+      case "javascript": {
+        return javascript;
+      }
+      case "json": {
+        return json;
+      }
+      case "sh":
+      case "shell": {
+        return shell;
+      }
+      case "gleam": {
+        return {
+          name: "gleam",
+          register: gleam
+        };
+      }
+      case "toml":
+      case "yaml": {
+        return yaml;
+      }
+      default: {
+        return plaintext;
+      }
+    }
+  };
 </script>
 
-<div class="py-10">
-  <HighlightAuto class="shadow-flat-green" code={text} />
+<div class="shadow-flat-green hljs divide-y-2 divide-gray-700">
+  {#if lang}
+    <aside class="px-4 py-2 text-gray-500 text-sm flex items-center justify-between">
+      {language}
+      {#if filename}
+        <span class="text-gray-600 text-xs">{filename}</span>
+      {/if}
+    </aside>
+  {/if}
+  {#if lang === "svelte"}
+    <HighlightSvelte code={text} let:highlighted>
+      <LineNumbers {highlighted} hideBorder />
+    </HighlightSvelte>
+  {:else}
+    <Highlight language={getLanguage()} code={text} let:highlighted>
+      <LineNumbers {highlighted} hideBorder />
+    </Highlight>
+  {/if}
 </div>
