655302d3a4724eeed421ab4ccaa4da215d181928
sneakycrow <zach@sneakycrow.dev>
2024-06-26 08:41:55 -0700

    add new image and copy to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,19 @@
   <title>sneaky crow</title>
 </svelte:head>
 
-<section class="col-start-1 lg:col-span-4 row-start-2 lg:row-span-2 max-w-[1000px] space-y-4">
-  <Profile />
+<section
+  class="col-start-1 lg:col-span-4 row-start-2 lg:row-span-2 max-w-[1000px] space-y-4 text-center"
+>
+  <img src="/ada_cropped.png" alt="Ada Lovelace" class="w-96 mx-auto" />
   <p class="text-xl">
     Hey there, I'm Zach. I'm a human being of many titles, and I'm definitely <span
       class="font-bold underline italic uppercase">not</span
-    > three crows in a long-coat.
+    > a crow in a long-coat pretending to be a human.
+  </p>
+  <p class="text-xl">
+    I like to write software, play video games, make music, read, and more! Check out my <a
+      href="/projects">projects</a
+    > for more.
   </p>
 </section>
 <ProjectList
