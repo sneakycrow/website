501f7d2395b86a9d184ce1e972abfb027610fdff
sneakycrow <zach@sneakycrow.dev>
2024-06-30 20:19:43 -0700

    add discord callbacks

---
+++ src/routes/login/discord/+server.ts
@@ -0,0 +1,23 @@
+import { redirect } from "@sveltejs/kit";
+import { generateState } from "arctic";
+import { discord } from "$lib/server/auth";
+
+import type { RequestEvent } from "@sveltejs/kit";
+import { scopes } from "$lib/server/discord";
+
+export async function GET(event: RequestEvent): Promise<Response> {
+  const state = generateState();
+  const url = await discord.createAuthorizationURL(state, {
+    scopes
+  });
+
+  event.cookies.set("discord_oauth_state", state, {
+    path: "/",
+    secure: import.meta.env.PROD,
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: "lax"
+  });
+
+  throw redirect(302, url.toString());
+}
