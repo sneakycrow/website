76d97c313051d44a125b20d7f6a53bccc23b362a
sneakycrow <zach@sneakycrow.dev>
2024-07-02 00:22:51 -0700

    fix thumbnails on mobile

--- src/lib/components/FilmPhoto.svelte
+++ src/lib/components/FilmPhoto.svelte
@@ -7,7 +7,7 @@
   };
 </script>
 
-<div class="flex flex-col justify-center items-center">
+<div class="flex flex-col justify-center items-center bg-black">
   <Thumbnail source={photo.source} alt={photo.alt} />
   <div class="flex flex-col w-full bg-black pb-6">
     <p class="text-sm italic text-white text-center">{photo.alt}</p>
