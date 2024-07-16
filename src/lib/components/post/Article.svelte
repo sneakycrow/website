038da8e62e1273f6329fa6299286da55ac8f1f4e
Zachary Corvidae <zach@sneakycrow.dev>
2024-07-16 13:55:08 -0700

    add z-index to draft text so it renders behind the article

--- src/lib/components/post/Article.svelte
+++ src/lib/components/post/Article.svelte
@@ -7,7 +7,9 @@
 </script>
 
 <div class={`grid grid-cols-4 items-start justify-center my-10 ${$$restProps.class} relative`}>
-  <article class="lg:text-lg w-full col-start-1 col-span-4 lg:col-span-3 max-w-[1000px] space-y-6">
+  <article
+    class="lg:text-lg w-full col-start-1 col-span-4 lg:col-span-3 max-w-[1000px] space-y-6 z-10"
+  >
     <a href={post.slug} class="text-black dark:text-white">
       <h2 class="text-3xl lg:text-4xl font-bold">{post.title}</h2>
     </a>
@@ -23,7 +25,7 @@
   </article>
   {#if post.draft}
     <p
-      class="fixed uppercase px-8 align-bottom text-error-300 text-9xl font-black leading-3 col-start-4 vertical-writing-lr"
+      class="z-0 fixed uppercase px-8 align-bottom text-error-100 text-9xl font-black leading-3 col-start-4 vertical-writing-lr"
     >
       Draft
     </p>
