ad0415cbd67c55f33d270b7c7138c77ba22c3104
Zachary Corvidae <zach@sneakycrow.dev>
2024-07-17 12:13:33 -0700

    add category list

---
+++ src/lib/components/post/CategoryList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import PostCategory from "./PostCategory.svelte";
+  import type { Category } from "$lib/posts";
+  export let categories: Category[] = [];
+  export let handleCategoryClick: (category: Category) => void;
+</script>
+
+<section class={`grid grid-cols-6 ${$$restProps.class}`}>
+  {#each categories as category}
+    <PostCategory {category} handleClick={handleCategoryClick} />
+  {/each}
+</section>
