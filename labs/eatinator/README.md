a1aa6f309923df77e1880f085f453ede602c9edc
Zachary Corvidae <zach@sneakycrow.dev>
2024-07-28 13:36:27 -0700

    remove rust eatinator

--- labs/eatinator/README.md
+++
@@ -1,6 +0,0 @@
-# eatinator
-
-eatinator is a data aggregation tool I built for myself. It's responsibility is to grab an account from my database, use that account to fetch data from a source, and then store that data.
-
-Sources:
-- [ ] [Spotify](https://www.spotify.com/)
--- labs/eatinator/src/vendor.rs
+++
@@ -1,9 +0,0 @@
-// Vendors are representations of third party services for which eatinator collects data
-// Each Vendor is provided with a means to access Accounts and Records in the database
-// On creation, a vendor is provided with a list of actions to take (data to collect) and a relative account
-//  to fetch that data with
-use crate::account::Account;
-
-pub(crate) trait Vendor {
-    fn account(&self) -> &Account;
-}
