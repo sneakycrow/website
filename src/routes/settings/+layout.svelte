fc9cbdbdad6b6a44cf2064e8053084b648f3d063
Zachary Corvidae <zach@sneakycrow.dev>
2024-08-27 16:24:27 -0700

    refactor settings/admin into a single route settings

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import Icon from "@iconify/svelte";
+  type PanelLink = { copy: string; url: string; id: string };
+  const panelLinks: PanelLink[] = [
+    { copy: "Users", url: "/settings/admin", id: "users" },
+    { copy: "Settings", url: "/settings", id: "settings" }
+  ];
+</script>
+
+<nav class="lg:col-span-1">
+  {#each panelLinks as panel}
+    <a
+      href={panel.url}
+      class="px-4 py-2 rounded-md flex flex-nowrap text-black dark:text-white"
+      class:bg-primary-500={$page.url.pathname === panel.url}
+    >
+      <Icon icon="mdi:account" class={`w-6 h-6`} />
+      {panel.copy}
+    </a>
+  {/each}
+</nav>
+
+<div class="table-container lg:col-span-5">
+  <slot />
+</div>
---
+++ src/routes/settings/admin/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { PageServerData } from "./$types";
+
+  export let data: PageServerData;
+  $: users = data?.users ?? [];
+</script>
+
+<table class="table table-hover bg-transparent">
+  <thead>
+    <tr>
+      {#each Object.keys(users[0]) as key}
+        <th>{key}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each users as user}
+      <tr>
+        {#each Object.values(user) as value}
+          <td>{value}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
