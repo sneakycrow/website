cf9a9ed880979a6e1f5cec78e6e5058b82c405a8
Zachary Corvidae <zach@sneakycrow.dev>
2024-08-27 15:32:16 -0700

    move photos to its own lib

---
+++ src/lib/photos.ts
@@ -0,0 +1,35 @@
+export const photoList = [
+  {
+    source: "/photos/start_family.jpeg",
+    alt: "Beginning of my family. Portland, OR",
+    date: "June 2017"
+  },
+  {
+    source: "/photos/timothy_lake.jpeg",
+    alt: "Timothy Lake. Mt. Hood National Forest, OR",
+    date: "June 2018"
+  },
+  {
+    source: "/photos/portland_new_years_2019.jpeg",
+    alt: "Portland New Years 2019. Portland, OR",
+    date: "January 2019"
+  },
+  {
+    source: "/photos/fence_flowers.jpeg",
+    alt: "Flowers behind a fence. Portland, OR",
+    date: "November 2020"
+  },
+  {
+    source: "/photos/arsenio.jpeg",
+    alt: "A dog named Arsenio. Portland, OR",
+    date: "November 2020"
+  },
+  {
+    source: "/photos/cape_disappointment.jpeg",
+    thumbnail: "/photos/cape_disappointment_thumbnail.jpeg",
+    alt: "Cape Disappointment. Ilwaco, WA",
+    date: "February 2020"
+  }
+];
+
+export default photoList;
