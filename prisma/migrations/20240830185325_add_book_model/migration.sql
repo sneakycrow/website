a4967bec81bb683d79099a971ab690be245cf7fc
Zachary Corvidae <zach@sneakycrow.dev>
2024-08-30 11:54:14 -0700

    add book model

---
+++ prisma/migrations/20240830185325_add_book_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "publishedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
