8d8f945055819768859a7a9783749b29fe7833f2
Zachary Corvidae <zach@sneakycrow.dev>
2024-09-17 14:03:14 -0700

    add quicklinks to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,26 @@
     style="animation: bounce 3s infinite;"
     width={300}
   />
+  <div class="flex space-x-4">
+    <a
+      href="https://github.com/sneakycrow"
+      class="text-sm uppercase text-black dark:text-white hover:text-blue-500 underline decoration-2 decoration-blue-500 transition-colors duration-300"
+    >
+      GitHub
+    </a>
+    <a
+      href="mailto:zach@sneakycrow.dev"
+      class="text-sm uppercase text-black dark:text-white hover:text-purple-500 underline decoration-2 decoration-purple-500 transition-colors duration-300"
+    >
+      Email
+    </a>
+    <a
+      href="/resume.pdf"
+      class="text-sm uppercase text-black dark:text-white hover:text-yellow-500 underline decoration-2 decoration-yellow-500 transition-colors duration-300"
+    >
+      Resume
+    </a>
+  </div>
   <p class="text-xl">
     Hey there, I'm Zach. I'm a human being of many titles, and I'm definitely <span
       class="font-bold underline italic uppercase">not</span
