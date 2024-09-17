d6823f3fa6a4ae5aaf8fa7d68ccc11f29bd38260
Zachary Corvidae <zach@sneakycrow.dev>
2024-09-17 13:47:00 -0700

    Use static components for all icons, remove Iconify library (#317)

--- src/lib/components/post/Image.svelte
+++ src/lib/components/post/Image.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Play from "../icons/Play.svelte";
+
   export let href: string;
   export let text: string;
 
@@ -6,15 +8,18 @@
   const isVideo = href.endsWith(".mp4") || href.endsWith(".webm");
 </script>
 
-<div class="w-full flex flex-col items-center justify-center space-y-4 py-4">
+<div class="w-full flex flex-col items-center justify-center space-y-4 relative">
   {#if isVideo}
-    <video controls class="shadow-flat-green max-w-[720px] w-full">
+    <video controls class="shadow-flat-green max-w-[720px] w-full z-10">
       <track kind="captions" />
       <source src={href} type={`video/${href.split(".").pop()}`} />
     </video>
+    <div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <Play width="4em" height="4em" />
+    </div>
     <p class="text-sm italic">{text}</p>
   {:else}
-    <img src={href} alt={text} class="shadow-xl" />
+    <img src={href} alt={text} class="shadow-flat-green" />
     <p class="text-sm italic">{text}</p>
   {/if}
 </div>
--- src/routes/settings/+layout.svelte
+++ src/routes/settings/+layout.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import Icon from "@iconify/svelte";
-  type PanelLink = { copy: string; url: string; id: string };
+  import Account from "$lib/components/icons/Account.svelte";
+  import Gear from "$lib/components/icons/Gear.svelte";
+  import { type ComponentType } from "svelte";
+  type PanelLink = { copy: string; url: string; id: string; icon: ComponentType };
   const panelLinks: PanelLink[] = [
-    { copy: "Users", url: "/settings/admin", id: "users" },
-    { copy: "Settings", url: "/settings", id: "settings" }
+    { copy: "Users", url: "/settings/admin", id: "users", icon: Account },
+    { copy: "Settings", url: "/settings", id: "settings", icon: Gear }
   ];
+
+  const ICON_SIZE = 24;
 </script>
 
 <nav class="lg:col-span-1">
   {#each panelLinks as panel}
     <a
       href={panel.url}
-      class="px-4 py-2 rounded-md flex flex-nowrap text-black dark:text-white"
+      class="px-4 py-2 rounded-md flex flex-nowrap text-black dark:text-white items-center space-x-2"
       class:bg-primary-500={$page.url.pathname === panel.url}
     >
-      <Icon icon="mdi:account" class={`w-6 h-6`} />
-      {panel.copy}
+      <svelte:component this={panel.icon} width={ICON_SIZE} height={ICON_SIZE} />
+      <span>{panel.copy}</span>
     </a>
   {/each}
 </nav>
