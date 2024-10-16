20d59f8e202ae908720f008a41870db9a773b30e
Zachary Corvidae <zach@sneakycrow.dev>
2024-10-16 01:01:02 -0700

    Remove email from footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -12,19 +12,19 @@
 </script>
 
 <footer
-  class={`py-4 flex flex-col space-y-10 lg:flex-row justify-between items-center z-10 ${$$restProps.class}`}
+  class={`py-4 flex flex-col lg:flex-row justify-between items-end z-10 ${$$restProps.class}`}
 >
   <p class="flex flex-col">
     <span class="text-sm">© {new Date().getFullYear()} Sneaky Crow, LLC</span>
     <span class="text-sm">All Rights Reserved</span>
     <a
-      class="text-gray-500 font-normal flex items-center space-x-2"
+      class="font-semibold flex items-center space-x-2"
       href="https://github.com/sneakycrow/website"
+      target="_blank"
     >
       <span>source code for this site</span>
       <GitHub class="inline-block w-4 h-4" />
     </a>
-    <a href="mailto:zach@sneakycrow.dev">contact me</a>
   </p>
   {#if user}
     <AvatarWidget {user} {links} />
