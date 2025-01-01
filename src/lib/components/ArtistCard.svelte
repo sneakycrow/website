256d7d80fd1277ae12ad17baba533ac973f4dbc6
Zachary Corvidae <zach@sneakycrow.dev>
2024-12-31 16:05:18 -0800

    Fix album card being stretched too much

--- src/lib/components/ArtistCard.svelte
+++ src/lib/components/ArtistCard.svelte
@@ -7,18 +7,16 @@
     height?: number;
   }
 
-  let {
-    url,
-    image,
-    name,
-    width = 300,
-    height = 300
-  }: Props = $props();
+  let { url, image, name, width = 300, height = 300 }: Props = $props();
 </script>
 
-<div class="space-y-2 flex flex-col items-center justify-center">
-  <a href={url} target="_blank" class="shadow-lg rounded-lg p-4 bg-black dark:bg-white">
-    <img src={image} alt={name} {width} {height} class="object-cover max-h-[300px] max-w-[300px]" />
+<div class="space-y-2 flex flex-col items-center justify-start">
+  <a
+    href={url}
+    target="_blank"
+    class="shadow-lg rounded-lg p-4 bg-black dark:bg-white max-w-[300px]"
+  >
+    <img src={image} alt={name} {width} {height} class="h-[300px] object-cover w-full" />
     <h3 class="font-bold text-xl text-primary-500 text-center mt-4">{name}</h3>
   </a>
 </div>
