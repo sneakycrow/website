d6a22d2a489a68a655027ab83455f058423e5dba
Zachary Corvidae <zach@sneakycrow.dev>
2025-02-09 14:10:04 -0800

    Add "today" copy to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
   import Collections from "$lib/components/Collections.svelte";
   import Socials from "$lib/components/Socials.svelte";
+
+  const creatorProjects = [
+    {
+      name: "Cobrashare",
+      link: "https://cobrashare.net",
+      description:
+        "A file-sharing platform for 3D artists to distribute files to their Patreon supporters"
+    },
+    {
+      name: "Farmhand",
+      link: "https://github.com/witchscrow/farmhand",
+      description:
+        "An open-source tool that helps livestreamers analyze and boost viewer engagement"
+    }
+  ];
 </script>
 
 <svelte:head>
@@ -8,7 +23,7 @@
 </svelte:head>
 
 <section
-  class="col-start-1 lg:col-span-6 min-h-[40vh] max-w-[1000px] mx-auto space-y-4 text-center flex flex-col justify-center items-center"
+  class="col-start-1 lg:col-span-6 min-h-[40vh] max-w-[1000px] mx-auto space-y-8 text-center flex flex-col justify-center items-center"
 >
   <img
     src="/ada_cropped.png"
@@ -22,6 +37,22 @@
       class="font-bold underline italic uppercase">not</span
     > a crow in a long-coat pretending to be a human.
   </p>
-  <p class="text-xl">I like to write software, play video games, make music, read, and more!</p>
+  <p class="text-lg">
+    Currently, I'm living in Portland, OR and working as a Senior Software Engineer at <a
+      href="https://www.laika.com/"
+      target="_blank">a Puppet Factory</a
+    > in addition to building custom projects for creators such as:
+  </p>
+  <ul class="flex justify-between items-start">
+    {#each creatorProjects as project (project.name)}
+      <li class="w-1/2 my-6 space-y-4 flex flex-col items-center text-center">
+        <span class="font-bold text-xl text-secondary-700 dark:text-secondary-100 uppercase"
+          ><a href={project.link} target="_blank">{project.name}</a></span
+        >
+        <span class="text-lg text-secondary-700 dark:text-secondary-300">{project.description}</span
+        >
+      </li>
+    {/each}
+  </ul>
 </section>
 <Collections class="lg:col-span-6 my-20" />
