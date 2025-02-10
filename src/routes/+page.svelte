71c2e9cb18a0e1f073e0182cec19d1e0db9259d9
Zachary Corvidae <zach@sneakycrow.dev>
2025-02-09 16:11:03 -0800

    Add Farmhand and Cobrashare icons to Quick Info (#354)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Collections from "$lib/components/Collections.svelte";
+  import Cobrashare from "$lib/components/icons/Cobrashare.svelte";
+  import Farmhand from "$lib/components/icons/Farmhand.svelte";
   import Socials from "$lib/components/Socials.svelte";
 
   const creatorProjects = [
@@ -7,13 +9,15 @@
       name: "Cobrashare",
       link: "https://cobrashare.net",
       description:
-        "A file-sharing platform for 3D artists to distribute files to their Patreon supporters"
+        "A file-sharing platform for 3D artists to distribute files to their Patreon supporters",
+      icon: Cobrashare
     },
     {
       name: "Farmhand",
       link: "https://github.com/witchscrow/farmhand",
       description:
-        "An open-source tool that helps livestreamers analyze and boost viewer engagement"
+        "An open-source tool that helps livestreamers analyze and boost viewer engagement",
+      icon: Farmhand
     }
   ];
 </script>
@@ -23,7 +27,7 @@
 </svelte:head>
 
 <section
-  class="col-start-1 lg:col-span-6 min-h-[40vh] max-w-[1000px] mx-auto space-y-8 text-center flex flex-col justify-center items-center"
+  class="col-start-1 lg:col-span-6 min-h-[40vh] max-w-[1000px] mx-auto space-y-10 text-center flex flex-col justify-center items-center"
 >
   <img
     src="/ada_cropped.png"
@@ -43,14 +47,20 @@
       target="_blank">a Puppet Factory</a
     > in addition to building custom projects for creators such as:
   </p>
-  <ul class="flex lg:flex-row flex-col justify-between lg:items-start items-center">
-    {#each creatorProjects as project (project.name)}
-      <li class="lg:w-1/2 my-6 space-y-4 flex flex-col items-center text-center">
-        <span class="font-bold text-xl text-secondary-700 dark:text-secondary-100 uppercase"
-          ><a href={project.link} target="_blank">{project.name}</a></span
+  <ul class="flex lg:flex-row flex-col justify-between lg:items-start items-center gap-6">
+    {#each creatorProjects as { link, name, description, icon: Icon }}
+      <li class="my-6 space-y-4 grid grid-rows-2">
+        <span
+          class="flex flex-row flex-nowrap items-center justify-center space-x-4 w-full lg:h-10"
         >
-        <span class="text-lg text-secondary-700 dark:text-secondary-300">{project.description}</span
-        >
+          <Icon />
+          <a
+            href={link}
+            class="font-bold text-xl text-secondary-700 dark:text-secondary-100 uppercase hover:text-primary-500"
+            target="_blank">{name}</a
+          >
+        </span>
+        <span class="text-lg text-secondary-700 dark:text-secondary-300">{description}</span>
       </li>
     {/each}
   </ul>
