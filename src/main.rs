69c8660d7042229819654a9a1601cf11f48be6c2
Zachary Corvidae <zach@sneakycrow.dev>
2025-11-29 23:33:23 -0800

    Add basic webserver support via axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::IntoResponse, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // Start the tracer
+    tracing_subscriber::fmt::init();
+
+    // Create the router
+    let app = Router::new().route("/", get(index));
+
+    // Initializer the listener
+    let address = "0.0.0.0:3000";
+    tracing::info!("Starting server on http://{}", address);
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server");
+}
+
+async fn index() -> impl IntoResponse {
+    "sneakycrow :)".into_response()
 }
