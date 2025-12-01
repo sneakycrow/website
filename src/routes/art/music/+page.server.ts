bc8b867dc8cbdd0dc8bacb83bad337954ec4b06f
Zachary Corvidae <zach@sneakycrow.dev>
2025-11-30 16:12:59 -0800

    Remove art page (temporarily)

--- src/routes/art/music/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async () => {
-  // Temporarily redirect to art music page until we finished the published music page
-  redirect(302, "/art/music/top-artists");
-};
